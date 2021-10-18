caebf38ce08db687a841265c979294d1a379406d
lloydnsiah <lloydnsiah@gmail.com>
2021-10-18 10:08:49 +0000

    updated layout

---
+++ app/src/main/java/com/example/launcher2/HiddenAppsAdapter.java
@@ -0,0 +1,82 @@
+package com.example.launcher2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HiddenAppsAdapter extends RecyclerView.Adapter<HiddenAppsAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<AppObject> arrayList;
+
+    public HiddenAppsAdapter(Context context, ArrayList<AppObject> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.new_layout_minus,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.name.setText(arrayList.get(position).getName());
+        holder.image.setImageDrawable(arrayList.get(position).getImage());
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Testing......", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.layout.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                holder.sub.setVisibility(View.VISIBLE);
+                return true;
+            }
+        });
+
+        holder.sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Add App To Showing Apps", Toast.LENGTH_SHORT).show();
+                holder.sub.setVisibility(View.INVISIBLE);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView image,sub;
+        private TextView name;
+        private LinearLayout layout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.allAppsImage);
+            name = itemView.findViewById(R.id.allAppsName);
+            layout= itemView.findViewById(R.id.allAppslayout);
+            sub =  itemView.findViewById(R.id.img_sub);
+
+        }
+    }
+}
